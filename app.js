9c25c438f0ff21c1e0d80dde83402ba50999c23b
xreyc <segumareyco@gmail.com>
2022-01-17 01:25:12 +0800

    update cors

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
 const express = require("express");
-var cors = require('cors');
+//var cors = require('cors');
 const app = express();
 
-app.use(cors({
-    origin: "https://main.d24qv3za7w153c.amplifyapp.com",
-    methods: ['GET','POST','DELETE','UPDATE','PUT','PATCH']
-}));
+// app.use(cors({
+//     origin: "https://main.d24qv3za7w153c.amplifyapp.com",
+//     methods: ['GET','POST','DELETE','UPDATE','PUT','PATCH']
+// }));
+
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', 'https://main.d24qv3za7w153c.amplifyapp.com');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+});
 
 require("./initialize/db")();
 require("./initialize/routes")(app);
