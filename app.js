f6a0992c2427292c7c9e30b6f2270080ea9eb2ec
xreyc <segumareyco@gmail.com>
2022-01-17 11:24:47 +0800

    heroku

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ const cors = require('cors');
 const app = express();
 
 app.use(cors({
-    origin: "https://main.d24qv3za7w153c.amplifyapp.com",
-    methods: ['GET','POST','DELETE','UPDATE','PUT','PATCH']
+    origin: "https://reyc-testapp4.herokuapp.com",
+    methods: ['GET','POST','PUT']
 }));
 
 // app.use(function (req, res, next) {
@@ -26,7 +26,7 @@ app.use(bodyParser.json());
 require("./initialize/routes")(app);
 
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 app.listen(port, () => {
     console.log(`Server is running at port ${port}`);
 })
--- routes/counterRoute.js
+++ routes/counterRoute.js
@@ -8,20 +8,25 @@ router.get("/", async (req, res) => {
 });
 
 router.post("/", async (req, res) => {
-    if(!req.body.id) {
-        let counter = new Counter({
+    let counter = new Counter({
+        count: req.body.count
+    });
+    
+    counter = await counter.save();
+    res.send(counter)
+});
+
+router.put("/:id", async (req, res) => {
+    const counter = await Counter.findByIdAndUpdate(
+        req.params.id,
+        {
             count: req.body.count
-        });
-        counter = await counter.save();
-        res.send(counter)
-    } else {
-        let counter = await Counter.findByIdAndUpdate(
-            req.body.id,
-            { count: req.body.count },
-            { new: true }
-        )
-        res.send(counter)
-    }
+        },
+        {
+            new: true
+        }
+    )
+    res.send(counter)
 });
 
 module.exports = router;
